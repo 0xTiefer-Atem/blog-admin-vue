00dd7a6270195b0c32232c85f728dd9564b3910b
0xMilk <1144502582@qq.com>
2020-05-29 16:26:32 +0800

    init

---
+++ src/components/manage/ManageBlog.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+<!--    面包屑导航区-->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item>文章管理</el-breadcrumb-item>
+    </el-breadcrumb>
+
+<!--    卡片视图区域-->
+    <el-card>
+      文章管理
+    </el-card>
+  </div>
+</template>
+
+<script>
+  import {request} from "../../network/request";
+
+  export default {
+    name: "BlogManage",
+    data() {
+      return {}
+    },
+
+    activated() {},
+
+    methods: {}
+  }
+</script>
+
+<style scoped>
+
+</style>
