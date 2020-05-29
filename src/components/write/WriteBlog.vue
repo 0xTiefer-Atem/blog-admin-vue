b2fd361b248942fed74a4566834b2e8f33d91a7f
0xMilk <1144502582@qq.com>
2020-05-29 16:26:52 +0800

    init

---
+++ src/components/write/WriteBlog.vue
@@ -0,0 +1,36 @@
+<template>
+  <div><!--    面包屑导航区-->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item>写文章</el-breadcrumb-item>
+    </el-breadcrumb>
+    <editor></editor>
+  </div>
+</template>
+
+<script>
+  import {request} from "../../network/request";
+  import Editor from "./editor/Editor";
+  export default {
+      name: "Case",
+      data() {
+          return {
+
+          }
+      },
+
+      activated() {
+
+      },
+
+
+      methods: {
+
+      },
+      components: {
+          Editor
+      }
+  }
+</script>
+
+<style scoped>
+</style>
