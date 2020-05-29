9e004e17a95a2f05e1f237222df9553a31095d95
0xMilk <1144502582@qq.com>
2020-05-29 16:26:39 +0800

    init

---
+++ src/components/personal/PrivateManage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <!--    面包屑导航区-->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item>个人信息管理</el-breadcrumb-item>
+    </el-breadcrumb>
+    <el-card>
+      个人信息管理
+    </el-card>
+  </div>
+</template>
+
+<script>
+  import {request} from "../../network/request";
+
+  export default {
+    name: "PrivateManage",
+    data() {
+      return {
+      }
+    },
+
+    //进入排号界面初始化
+    activated() {},
+    methods: {}
+  }
+</script>
+
+<style scoped>
+
+</style>
