a36d60ac36e5cf41d92f418a6bb6009bd3d4a7e9
0xMilk <1144502582@qq.com>
2020-05-31 16:45:31 +0800

    添加代理服务

--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,13 @@ module.exports = {
   // 选项...
   devServer: {
     port: 8080,
-    // proxy: {
-    //   '/api': { // 匹配所有以 '/api'开头的请求路径
-    //     target: 'http://47.107.64.157:9090/api', // 代理目标的基础路径
-    //     // secure: false,  // 如果是https接口，需要配置这个参数
-    //     ws: true,
-    //     changeOrigin: true, // 支持跨域
-    //   }
-    // }
+    proxy: {
+      '/api': { // 匹配所有以 '/api'开头的请求路径
+        target: 'http://localshot:9192/api', // 代理目标的基础路径
+        // secure: false,  // 如果是https接口，需要配置这个参数
+        ws: true,
+        changeOrigin: true, // 支持跨域
+      }
+    }
   }
 }
