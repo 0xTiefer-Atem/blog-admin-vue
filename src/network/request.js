f05d0afb8b6cb290666d24d57851fb9ff4494ebd
0xMilk <1144502582@qq.com>
2020-05-31 16:45:03 +0800

    修改基本请求路径

--- src/network/request.js
+++ src/network/request.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 export function request(config) {
   //1. 创建axios实例
   const instance = axios.create({
-    baseURL: 'http://47.107.64.157:8080/api',
+    baseURL: 'http://localhost:9192/api',
     // timeout: 5000,
 
     //跨域
