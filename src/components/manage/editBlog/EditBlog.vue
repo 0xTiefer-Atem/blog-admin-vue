0939ac97dbff8458706e2fe33acbe8cf70423cc3
0xMilk <1144502582@qq.com>
2020-05-31 16:43:29 +0800

    将文章管理分为两个组件

---
+++ src/components/manage/editBlog/EditBlog.vue
@@ -0,0 +1,28 @@
+<template>
+  <el-card class="ql-editor-card">
+    <editor></editor>
+  </el-card>
+</template>
+
+<script>
+  import Editor from "../../common/Editor";
+    export default {
+        name: "EditBlog",
+        components: {Editor},
+        data() {
+            return {
+
+            }
+        },
+        methods: {
+
+        },
+        comments: {
+            Editor
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
