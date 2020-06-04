9ed5622195a3b0815e2f14853299e21457b5cf53
0xMilk <1144502582@qq.com>
2020-06-04 23:39:35 +0800

    父子组件传值

--- src/components/manage/editBlog/EditBlog.vue
+++ src/components/manage/editBlog/EditBlog.vue
@@ -1,20 +1,26 @@
 <template>
-    <editor></editor>
+    <editor :blog="blog"></editor>
 </template>
 
 <script>
   import Editor from "../../common/Editor";
     export default {
         name: "EditBlog",
-        components: {Editor},
+        components: {
+            Editor
+        },
+        created() {
+            this.blog.blogId = this.$route.query.blogId
+            console.log(this.blog);
+        },
         data() {
             return {
+                blog: {
 
+                }
             }
         },
-        methods: {
-
-        },
+        methods: {},
         comments: {
             Editor
         }
