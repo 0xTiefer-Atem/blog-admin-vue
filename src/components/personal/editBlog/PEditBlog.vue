0c99fae32b4750e9bb9f8933a8793951191f45c9
0xMilk <1144502582@qq.com>
2020-06-26 20:07:21 +0800

    添加新页面

---
+++ src/components/personal/editBlog/PEditBlog.vue
@@ -0,0 +1,32 @@
+<template>
+    <editor></editor>
+</template>
+
+<script>
+  import Editor from "../../common/Editor";
+    export default {
+        name: "PEditBlog",
+        components: {
+            Editor
+        },
+        activated() {
+            let blog = {}
+            blog.blogId = this.$route.query.blogId
+            blog.blogStatus = this.$route.query.blogStatus
+            this.$store.commit({
+                type: 'updateBlogInfo',
+                blog
+            })
+        },
+        methods: {
+
+        },
+        comments: {
+            Editor
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
