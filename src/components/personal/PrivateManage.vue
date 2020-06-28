ca1abaedb6904a34a36fca3a9947403a6abace64
0xMilk <1144502582@qq.com>
2020-06-28 09:35:38 +0800

    增加keep-alive标签

--- src/components/personal/PrivateManage.vue
+++ src/components/personal/PrivateManage.vue
@@ -4,25 +4,16 @@
     <el-breadcrumb separator-class="el-icon-arrow-right">
       <el-breadcrumb-item>个人信息管理</el-breadcrumb-item>
     </el-breadcrumb>
-    <el-card>
-      个人信息管理
-    </el-card>
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
   </div>
 </template>
 
 <script>
-  import {request} from "../../network/request";
 
   export default {
     name: "PrivateManage",
-    data() {
-      return {
-      }
-    },
-
-    //进入排号界面初始化
-    activated() {},
-    methods: {}
   }
 </script>
 
