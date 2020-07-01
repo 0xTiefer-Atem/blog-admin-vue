493183b4080a03001d67aafef98f172c89300ca3
0xMilk <1144502582@qq.com>
2020-07-01 18:58:03 +0800

    添加代码高亮引用

--- src/main.js
+++ src/main.js
@@ -5,6 +5,10 @@ import 'element-ui/lib/theme-chalk/index.css'
 import router from './router/index'
 import store from "./store";
 
+// highlight.js代码高亮插件
+import Highlight from './assets/js/highlight'; // from 路径是highlight.js的路径，纯属自定义
+Vue.use(Highlight);
+
 
 //导入全局样式表
 import './assets/css/global.css'
