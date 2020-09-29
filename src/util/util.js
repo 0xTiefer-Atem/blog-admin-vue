f96aaf23be367e4874a3e7b7f2310d5eea442908
Wq <1144502582@qq.com>
2020-09-30 00:25:05 +0800

    引入时间格式化工具

---
+++ src/util/util.js
@@ -0,0 +1,10 @@
+function formatDate(value) {
+    let date = new Date(value);
+    let year = date.getFullYear();
+    let month = padDate(date.getMonth()+1);
+    let day = padDate(date.getDate());
+    let hours = padDate(date.getHours());
+    let minutes = padDate(date.getMinutes());
+    let seconds = padDate(date.getSeconds());
+    return year + '-' + month + "-" + day + "  " + hours + ":" + minutes + ":" + seconds
+}
