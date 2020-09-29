982b123ee4e226a15287560b85a863b6bbd3186b
Wq <1144502582@qq.com>
2020-09-30 00:25:25 +0800

    引入mavonEditor编辑器

--- src/main.js
+++ src/main.js
@@ -5,17 +5,27 @@ import 'element-ui/lib/theme-chalk/index.css'
 import router from './router/index'
 import store from "./store";
 
-// highlight.js代码高亮插件
-import Highlight from './assets/js/highlight'; // from 路径是highlight.js的路径，纯属自定义
-Vue.use(Highlight);
-
-
 //导入全局样式表
 import './assets/css/global.css'
 
+import mavonEditor from 'mavon-editor'
+import 'mavon-editor/dist/css/index.css'
+
+// highlight.js代码高亮插件
+import Highlight from './assets/js/highlight'; // from 路径是highlight.js的路径，纯属自定义
+
+
+
+
+
+
+
 
 Vue.use(ElementUI);
+Vue.use(mavonEditor)
+Vue.use(Highlight);
 Vue.config.productionTip = false;
+
 
 new Vue({
   router,
