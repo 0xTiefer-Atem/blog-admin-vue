d5b6c24d1ef8185aaa0057e651e3f8c099517e10
Wq <1144502582@qq.com>
2020-10-01 21:10:15 +0800

    修改登录界面图片路径

--- src/components/login/Login.vue
+++ src/components/login/Login.vue
@@ -3,7 +3,7 @@
     <div class="login_box">
       <!--      头像区域-->
       <div class="avatar_box">
-        <img src="../../assets/logo.png" alt="">
+        <img src="http://47.107.64.157/user/avatar/tx1.jpeg" alt="">
       </div>
       <div style="margin-bottom: 10px">
         <h3 class="title">个人博客管理系统</h3>
