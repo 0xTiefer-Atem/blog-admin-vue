34dbfda32ce8a4376d61cb2b87f8e98433216ce5
Wq <1144502582@qq.com>
2020-10-01 21:11:28 +0800

    增加个人信息展示界面

--- src/components/personal/privateInfoCard/PrivateInfoCard.vue
+++ src/components/personal/privateInfoCard/PrivateInfoCard.vue
@@ -2,6 +2,38 @@
   <div>
     <el-card class="personal-card" shadow="never">
       <el-row :gutter="24">
+        <!--个人信息-->
+        <el-col :span="8" :offset="2">
+          <el-card :body-style="{ padding: '0px' }"
+                   class="article-card"
+                   shadow="hover">
+            <img src="https://cube.elemecdn.com/6/94/4d3ea53c084bad6931a56d5158a48jpeg.jpeg" class="image">
+            <el-row>
+              <el-col>
+                <div>
+                  <el-card class="article-info-card" @click.native="personalInfoClick" shadow="never">
+                    <div slot="header" class="article-title">
+                      0xTiefer_Atem
+                    </div>
+                    <div class="article-cover-text">
+                      Technology changes the world
+                    </div>
+                    <el-divider></el-divider>
+                    <el-row :grunt="24">
+                      <el-col :span="12" :offset="12">
+                        <div>
+                          <el-tag>2020-05-27 10:42:23</el-tag>
+                        </div>
+                      </el-col>
+                    </el-row>
+                  </el-card>
+                </div>
+              </el-col>
+            </el-row>
+          </el-card>
+        </el-col>
+
+        <!--个人简历，教育经历-->
         <el-col :span="8" :offset="2" v-for="(info, index) in personalInfoList" :key="index">
           <el-card :body-style="{ padding: '0px' }"
                    class="article-card"
@@ -67,7 +99,6 @@ export default {
       return moment(date).format("YYYY-MM-DD HH:mm")
     },
     personalDetailClick(info) {
-      console.log(info)
       let query = {}
       query.blogId = info.blogId
       query.blogStatus = info.blogStatus
@@ -75,15 +106,15 @@ export default {
         path: "/blog-admin/private-manage/private-edit",
         query
       });
+    },
+    personalInfoClick() {
+      this.$router.push('/blog-admin/private-manage/personal-detail')
     }
   }
 }
 </script>
 
 <style scoped>
-.personal-card {
-  /*border-radius: 30px;*/
-}
 
 .image {
   width: 100%;
@@ -93,10 +124,6 @@ export default {
 .article-card {
   border-radius: 30px;
   margin: 10px;
-}
-
-.article-list-card {
-  border-radius: 30px;
 }
 
 .article-cover-text {
